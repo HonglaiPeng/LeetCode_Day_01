c94900640b9d5b1066f353c5d8c8fcf1e88bf54b
HonglaiPeng <honglaipeng007@gmail.com>
2021-07-12 17:17:52 -0400

    Update 26. Remove Duplicates from Sorted Array.java

--- 26. Remove Duplicates from Sorted Array.java
+++ 26. Remove Duplicates from Sorted Array.java
@@ -1,7 +1,7 @@
 class Solution {
     // here we use two pointers method
-    // Time complexity:
-    // Space complexity:
+    // Time complexity: O(n)
+    // Space complexity: O(1)
     public int removeDuplicates(int[] nums) {
         int nextNonDuplicate = 1;
         for(int i = 1; i < nums.length; i++){
